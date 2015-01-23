391a559227e86b920734f5ef6072b483f0bc5617
czhang <naiqiang@yahoo.com>
2015-01-22 19:05:50 -0800

    added a NSLog

--- class2-xgitTests/class2_xgitTests.m
+++ class2-xgitTests/class2_xgitTests.m
@@ -17,6 +17,9 @@
 
 - (void)setUp {
     [super setUp];
+    
+    NSLog(@"test set up");
+    
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
